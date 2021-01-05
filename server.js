0d80188f517b82238a3eb2590a47f9f4afe86c38
unknown <csecec.1703176@gmail.com>
2021-01-05 16:59:26 +0530

    make it better

--- server.js
+++ server.js
@@ -2,13 +2,20 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const path = require('path');
-
+const cors= require('cors');
 const PORT=process.env.PORT || 3000;
 app.use(express.static('public'));
 app.use(express.json());
 const connectDB = require('./config/db');
 
+const corsOptions = {
+    origin: process.env.ALLOWED_CLIENTS.split(',')
+    // ['http://localhost:3000', 'http://localhost:5000', 'http://localhost:3300']
+  }
+app.use(cors(corsOptions));
 connectDB();
+
+
 //template engines
 app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'ejs');
